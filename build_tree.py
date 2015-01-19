0624f971ea22fed389e2f7d26d9980b1d19bc3e1
pmezentsev <pmezentsev@tsb-hadoop.(none)>
2015-01-19 21:56:44 +0100

    example was edited

--- build_tree.py
+++ build_tree.py
@@ -1,9 +1,15 @@
 import id3
+from pyspark import SparkContext, SparkConf
 
-input_rrd = sc.textFile ("/testdata/kaggle")
+sc = SparkContext ()
+input_rdd = sc.textFile ("/testdata/kaggle")
 data_step1_rdd = input_rdd.map (lambda x: x.split("\t"))
-data_rdd = data_step1_rdd.map (lambda x: {"f":map (lambda y: float(y), x[1:]), "t": int (x[0]), "w":1})
-
+data_rdd = data_step1_rdd.map (lambda x: id3.DataPoint (features = map (lambda y: float(y), x[1:]),
+                                                        target   = int (x[0]),
+                                                        weight   = 1))
 
 tree  = id3.id3 (data_rdd)
 
+for element in data_rdd.take(10):
+    result = tree.evaluate (element.features)
+    print "expected result", element.target,  "classification result:", result
